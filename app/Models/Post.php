7a10c9979b37395be6bd0c21c722516fb43053bd
João Damaceno <joaodamaceno188@gmail.com>
2023-06-21 14:26:45 -0400

    patch: :recycle: relaciona user e post.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * Retorna Relacionamento com post ao usuario.
+     * @return BelongsTo
+     */
+    final public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
