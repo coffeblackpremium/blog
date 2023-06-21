53641719ab27b25436e172d38ef0a4a40725b391
João Damaceno <joaodamaceno188@gmail.com>
2023-06-21 14:34:20 -0400

    patch: :recycle: remove metodos finais.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,28 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class PostController extends Controller
 {
-    final public function index()
+    public function index()
     {
         //
     }
 
-    final public function create(): Response
+    public function store(Request $request, Post $posts): RedirectResponse
+    {
+        $validate = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+            'user_id' => 'required|exists:users,id',
+            'slug' => 'required',
+        ]);
+
+        $posts->create($validate);
+
+        return redirect()->to(route('dashboard'));
+    }
+
+    public function create(): Response
     {
         return Inertia::render('Post/Create');
     }
 
-    public function store(Request $request)
-    {
-        //
-    }
-
-    final public function show($id)
+    public function show($id)
     {
         //
     }
