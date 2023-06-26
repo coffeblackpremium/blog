7157680c5dbfcc64aaaf1036bbf15fdc8df87662
João Damaceno <joaodamaceno188@gmail.com>
2023-06-26 11:08:29 -0400

    feat: :sparkles: adiciona observer para criar slug.

---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+
+class PostObserver
+{
+    public function created(Post $post): void
+    {
+        $post->update(['slug' => Str::slug($post->title, '-')]);
+
+        $post->save();
+    }
+
+    public function updated(Post $post): void
+    {
+    }
+
+    public function deleted(Post $post): void
+    {
+    }
+
+    public function restored(Post $post): void
+    {
+    }
+
+    public function forceDeleted(Post $post): void
+    {
+    }
+}
