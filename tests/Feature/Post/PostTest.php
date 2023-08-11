81fa577872b9969f48bed765f972305492a82a10
João Damaceno <joaodamaceno188@gmail.com>
2023-08-11 16:17:39 -0400

    refactor: white_check_mark: testa autenticação, criacao de posts e componente para posts

--- tests/Feature/Post/PostTest.php
+++ tests/Feature/Post/PostTest.php
@@ -5,9 +5,20 @@ use App\Events\NewPostCreatedEvent;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Inertia\Testing\AssertableInertia as Assert;
 use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\get;
 
-it('can create a new post in blog', function () {
+it('should be have a component to create posts', function () {
+    $this->actingAs(User::factory()->create())
+    ->get(route('posts.create'))
+    ->assertOk()
+    ->assertInertia(fn(Assert $page) => $page
+        ->component('Post/Create')
+    );
+});
+
+it('should be create a new post in blog', function () {
     //Arrange
     $user = User::factory()->create();
 
@@ -26,4 +37,9 @@ it('can create a new post in blog', function () {
             'slug' => Str::slug($title),
             'user_id' => $user->id,
         ]));
+});
+
+it('guests can not create new post', function() {
+    get(route('posts.create'))
+        ->assertForbidden();
 });
