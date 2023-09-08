b96e9362acd4dd502abebc2e6ad7599e03608d85
João Damaceno <joaodamaceno188@gmail.com>
2023-09-08 00:43:04 -0400

    patch: :recycle: configura meilisearch.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Scout\Attributes\SearchUsingFullText;
+use Laravel\Scout\Attributes\SearchUsingPrefix;
 use Laravel\Scout\Searchable;
 
 class Post extends Model
@@ -15,6 +17,21 @@ class Post extends Model
     protected $casts = [
       'created_at' => 'datetime:Y-m-d',
     ];
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array<string, mixed>
+     */
+    #[SearchUsingPrefix(['title'])]
+    #[SearchUsingFullText(['body'])]
+    public function toSearchableArray(): array
+    {
+        return [
+            'title' => $this->title,
+            'body' => $this->body
+        ];
+    }
 
     /**
      * Retorna Relacionamento com post ao usuario.
