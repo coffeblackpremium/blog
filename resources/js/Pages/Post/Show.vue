f5af9ddcabc6ad566ea34600aebc44a6f6c823e0
João Damaceno <joaodamaceno188@gmail.com>
2024-01-29 16:30:11 -0400

    feat: criação do rodape do site.

--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -24,8 +24,8 @@ const markedBody = computed(() => {
                 <h1 class="font-bold md:max-w-xl text-4xl mb-1.5 font-sans tracking-wide break-keep">{{ post.title }}</h1>
                 <div class="text-start text-pretty py-2 font-roboto md:max-w-full">
                     <div class="pt-3">
-                        <span class="italic">
-                            Tempo de Leitura: <span class="font-bold">{{ readingPace <= 1 ?  readingPace + " minuto" : readingPace + " Minutos" }} </span>
+                        <span class="roboto">
+                            Tempo de Leitura: <span class="italic font-bold text-neutral-900 dark:text-white">{{ readingPace <= 1 ?  readingPace + " minuto" : readingPace + " Minutos" }} </span>
                         </span>
                     </div>
                     <div class="pt-2">
@@ -41,9 +41,10 @@ const markedBody = computed(() => {
                     </div>
                 </div>
             </div>
+
         </div>
-        <div class="bg-neutral-300 border rounded-sm">
-            <div class="flex flex-col md:max-w-2xl justify-center items-center mx-auto">
+        <div class="bg-neutral-300 border rounded-sm dark:bg-slate-800 dark:border-black">
+            <div class="flex flex-col md:max-w-3xl justify-center items-center mx-auto">
                 <div class="flex items-center ">
                     <span class="text-3xl roboto mt-2"> Gostou do conteúdo ?</span>
                 </div>
@@ -58,12 +59,23 @@ const markedBody = computed(() => {
                             <font-awesome-icon icon="fa-brands fa-linkedin-in"></font-awesome-icon>
                         </a>
                     </div>
-                    <div class="md:mx-3">
+                    <div class="md:mx-3 ">
                         <a href="https://github.com/coffeblackpremium">
                             <font-awesome-icon icon="fa-brands fa-github" />
                         </a>
                     </div>
                 </div>
+            </div>
+            <div class="mx-auto md:max-w-3xl">
+                <hr style="height:1px;border-width:0;color:gray;background-color:gray"/>
+                <span class="flex text-sm justify-center mt-3">
+                        Desenvolvido por: <span class="italic"> João Damaceno </span>
+                    </span>
+            </div>
+            <div class="flex flex-row-reverse ">
+                    <span class="text-sm md:mr-4">
+                        © Todos os direitos reservados
+                    </span>
             </div>
         </div>
     </div>
