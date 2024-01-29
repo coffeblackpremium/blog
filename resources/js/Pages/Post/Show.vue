aa2010204a1c04351bae27dae0c5579e715a1cb6
João Damaceno <joaodamaceno188@gmail.com>
2024-01-29 15:50:08 -0400

    wip

--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -2,11 +2,16 @@
 import Navbar from "@/Components/Navbar.vue";
 import {computed} from "vue";
 import {marked} from "marked";
+import {Post} from "@/Interfaces/Posts";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
-const post = defineProps({post: Object, readingPace: Number});
+const props = defineProps<{
+    post: Post,
+    readingPace: Number
+}>();
 
 const markedBody = computed(() => {
-    return marked(post.post.body);
+    return marked(props.post.body);
 });
 
 </script>
@@ -14,18 +19,18 @@ const markedBody = computed(() => {
 <template>
     <Navbar/>
     <div class="bg-neutral-50 dark:bg-gray-900">
-        <div class="mx-auto md:max-w-3xl max-w-sm py-24 flex justify-center">
+        <div class="md:max-w-3xl max-w-sm py-24 flex justify-center items-center mx-auto">
             <div class="flex flex-col">
-                <h1 class="font-bold md:max-w-xl text-4xl mb-1.5 font-sans tracking-wide break-keep">{{ post.post.title }}</h1>
+                <h1 class="font-bold md:max-w-xl text-4xl mb-1.5 font-sans tracking-wide break-keep">{{ post.title }}</h1>
                 <div class="text-start text-pretty py-2 font-roboto md:max-w-full">
                     <div class="pt-3">
                         <span class="italic">
-                            Tempo de Leitura: <span class="font-bold">{{ post.readingPace <= 1 ?  post.readingPace + " minuto" : post.readingPace + " Minutos" }} </span>
+                            Tempo de Leitura: <span class="font-bold">{{ readingPace <= 1 ?  readingPace + " minuto" : readingPace + " Minutos" }} </span>
                         </span>
                     </div>
                     <div class="pt-2">
                         <span class="italic text-bold">
-                            Criado: <span class="font-bold">{{ post.post.created_at }} </span>
+                            Criado: <span class="font-bold">{{ post.created_at }} </span>
                         </span>
                     </div>
                     <hr class="mt-4">
@@ -36,11 +41,29 @@ const markedBody = computed(() => {
                     </div>
                 </div>
             </div>
-
         </div>
-        <div class="bg-white border rounded-sm">
-            <div class="flex flex-col md:max-w-3xl justify-items-center mx-auto">
-                <h1>Hello World</h1>
+        <div class="bg-neutral-300 border rounded-sm">
+            <div class="flex flex-col md:max-w-2xl justify-center items-center mx-auto">
+                <div class="flex items-center ">
+                    <span class="text-3xl roboto mt-2"> Gostou do conteúdo ?</span>
+                </div>
+                <div class="flex items-cecnter">
+                    <span class="text-xl roboto italic">
+                        Me siga nas redes sociais
+                    </span>
+                </div>
+                <div class="flex items-center justify-center fa-3x">
+                    <div class="md:mx-3">
+                        <a href="https://www.linkedin.com/in/joao-damaceno/">
+                            <font-awesome-icon icon="fa-brands fa-linkedin-in"></font-awesome-icon>
+                        </a>
+                    </div>
+                    <div class="md:mx-3">
+                        <a href="https://github.com/coffeblackpremium">
+                            <font-awesome-icon icon="fa-brands fa-github" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
