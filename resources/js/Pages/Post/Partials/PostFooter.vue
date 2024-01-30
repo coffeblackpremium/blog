7b1b40c7efb13f4a61d699294b90c65cb9d6c087
João Damaceno <joaodamaceno188@gmail.com>
2024-01-29 21:02:37 -0400

    refactor: correção de tailwind.

--- resources/js/Pages/Post/Partials/PostFooter.vue
+++ resources/js/Pages/Post/Partials/PostFooter.vue
@@ -5,12 +5,14 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 <template>
     <div class="flex flex-col md:max-w-3xl justify-center items-center mx-auto">
         <div class="flex items-center mt-4">
-            <span class="text-3xl roboto mt-2"> Gostou do conteúdo ?</span>
+            <span class="text-3xl roboto mt-2">
+                Gostou do conteúdo ?
+            </span>
         </div>
-        <div class="flex items-cecnter">
-                    <span class="text-xl roboto italic">
-                        Me siga nas redes
-                    </span>
+        <div class="flex items-center">
+            <span class="text-xl roboto italic">
+                Me siga nas redes
+            </span>
         </div>
         <div class="flex items-center justify-center fa-3x">
             <div class="mx-3">
@@ -20,7 +22,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
             </div>
             <div class="mx-3 ">
                 <a href="https://github.com/coffeblackpremium">
-                    <font-awesome-icon icon="fa-brands fa-github" />
+                    <font-awesome-icon icon="fa-brands fa-github"/>
                 </a>
             </div>
         </div>
@@ -28,16 +30,16 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
     <div class="mx-auto md:max-w-3xl">
         <hr style="height:1px;border-width:0;color:gray;background-color:gray"/>
         <span class="flex text-sm justify-center mt-3 ">
-                        Desenvolvido por: <span class="italic"> João Damaceno </span>
-                    </span>
+            Desenvolvido por: <span class="italic"> João Damaceno </span>
+        </span>
     </div>
     <div class="flex flex-row-reverse ">
-                <span class="hidden text-sm justify-start">
-                    Desenvolvido por: <span class="italic"> João Damacenoooo </span>
-                </span>
+        <span class="hidden text-sm justify-start">
+            Desenvolvido por: <span class="italic"> João Damaceno</span>
+        </span>
         <span class="text-sm md:mr-4 mt-4">
-                        © Todos os direitos reservados
-                    </span>
+            © Todos os direitos reservados
+        </span>
     </div>
 </template>
 
